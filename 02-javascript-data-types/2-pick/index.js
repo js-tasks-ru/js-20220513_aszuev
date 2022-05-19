acc96c50c8a0dc2dd2f76730165542215b2e1421
Alexanderzu <artminor@yandex.ru>
2022-05-19 15:37:50 +0400

    02-2

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+    let fruitsArr = [];
+  
+    for (const [key, value] of Object.entries(fields)) {
+        const fruitKey = fields[key];
+        const fruitValue = parseInt(obj[value]);
+        fruitsArr.push([fruitKey,fruitValue]);
+    }
 
+    fruitsArr = Object.fromEntries(fruitsArr)
+    return fruitsArr
 };
